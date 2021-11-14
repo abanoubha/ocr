b8ce4db0dd71f5864cc952b134659720eb78b43e
Abanoub Hanna <abanoubhannaa73@gmail.com>
2021-11-14 03:46:38 +0200

    steep starting

---
+++ src/go.mod
@@ -0,0 +1,10 @@
+module github.com/abanoubhannaazer/go-ocr
+
+go 1.17
+
+require github.com/otiai10/gosseract v2.2.1+incompatible
+
+require (
+	github.com/otiai10/mint v1.3.3 // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+)
---
+++ src/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/otiai10/gosseract"
+)
+
+// ocr --lang=ara --img=xyz.png
+func main() {
+	lang := flag.String("lang", "eng", " Language of the written text. eng or ara as a language.")
+	img := flag.String("img", "", " Image.")
+	flag.Parse()
+
+	if *img == "" {
+		fmt.Println("Usage : ocr --lang=eng --img=~/Downloads/xyz.png")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	if *lang != "eng" && *lang != "ara" {
+		fmt.Println("--lang must be ara for Arabic or eng for English.")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	fmt.Println(*lang, *img)
+	extracted, err := ocr(*img, *lang)
+	if err != nil {
+		fmt.Println("Error : ", err.Error())
+	}
+	fmt.Println(extracted)
+}
+
+func ocr(imgpath, lang string) (string, error) {
+	client := gosseract.NewClient()
+	defer client.Close()
+
+	// client.Languages = []string{"eng", "ara"}
+	client.SetLanguage(lang)
+
+	client.SetImage(imgpath)
+
+	//boundingBox, _ := client.GetBoundingBoxes(PageIteratorLevel.RIL_SYMBOL)
+
+	text, err := client.Text()
+	// text, err := client.HOCRText()
+	if err != nil {
+		return text, nil
+	}
+	return "", err
+}
