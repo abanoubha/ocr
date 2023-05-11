965a6fd8a33b44645bb6e64b81aef8494ddf88f9
Abanoub Hanna <abanoubhannaa73@gmail.com>
2023-05-11 03:57:36 +0300

    custom simple threshold func

--- src/go.mod
+++ src/go.mod
@@ -4,10 +4,7 @@ go 1.17
 
 require (
 	github.com/disintegration/imaging v1.6.2
-	github.com/otiai10/gosseract/v2 v2.3.1
+	github.com/otiai10/gosseract/v2 v2.4.0
 )
 
-require (
-	github.com/otiai10/mint v1.3.3 // indirect
-	golang.org/x/image v0.0.0-20220617043117-41969df76e82 // indirect
-)
+require golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
--- src/main.go
+++ src/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"image"
+	"image/color"
 	"os"
 
 	"github.com/disintegration/imaging"
@@ -12,6 +13,19 @@ import (
 
 // ocr --lang=ara --img=xyz.png
 func main() {
+
+	// A/B Testing :
+	// - use custom threshold vs the default threshold in Tesseract
+	// - determine the better result by the overall confidence
+	//
+	// osf, _ := os.Open("../img/testar.jpg")
+	// defer osf.Close()
+	// omg, _ := jpeg.Decode(osf)
+	// // result := OtsuThreshold(omg)
+	// result := threshold(omg, (255/2)+50)
+	// ff, _ := os.Create("result.jpg")
+	// jpeg.Encode(ff, result, &jpeg.Options{Quality: 95})
+
 	lang := flag.String("lang", "eng", " Language of the written text. eng or ara as a language.")
 	img := flag.String("img", "", " Image.")
 	flag.Parse()
@@ -71,4 +85,101 @@ func ocr(imgpath, lang string, isBlackBg bool) (string, error) {
 		return "", err
 	}
 	return text, nil
+} // ocr
+
+func threshold(img image.Image, threshold float64) image.Image {
+	// Get the image dimensions.
+	width, height := img.Bounds().Dx(), img.Bounds().Dy()
+
+	// Create a new image with the same dimensions as the original image.
+	thresholded := image.NewGray(img.Bounds())
+
+	// Set the pixels to black if they are below the threshold, and white otherwise.
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			// Get the pixel value.
+			// v := img.At(x, y).Y
+			r, g, b, _ := img.At(x, y).RGBA()
+			v := float64(r+g+b) / (3 * 255)
+
+			// Set the pixel.
+			if v < threshold {
+				thresholded.Set(x, y, color.Black)
+			} else {
+				thresholded.Set(x, y, color.White)
+			}
+		}
+	}
+
+	// Return the thresholded image.
+	return thresholded
 }
+
+// func OtsuThreshold(img image.Image) image.Image {
+// 	// Get the image dimensions.
+// 	width, height := img.Bounds().Dx(), img.Bounds().Dy()
+
+// 	// Create a new image with the same dimensions as the original image.
+// 	thresholded := image.NewGray(img.Bounds())
+
+// 	// Calculate the Otsu threshold.
+// 	var total int64
+// 	var sum float64
+// 	var w0, w1 float64
+// 	var u0, u1 float64
+// 	var var0, var1 float64
+// 	for y := 0; y < height; y++ {
+// 		for x := 0; x < width; x++ {
+// 			// Get the pixel value.
+// 			// v := img.At(x, y).Y
+// 			r, g, b, _ := img.At(x, y).RGBA()
+// 			// v := float64(r+g+b) / (3 * 255)
+// 			const rConst float64 = 0.2126
+// 			const gConst float64 = 0.7152
+// 			const bConst float64 = 0.0722
+// 			v := (rConst*float64(r) + gConst*float64(g) + bConst*float64(b)) / 255
+
+// 			// Update the total, sum, w0, w1, u0, u1, var0, and var1 variables.
+// 			total += 1
+// 			sum += v
+// 			w0 += v < 0.5
+// 			w1 += v >= 0.5
+// 			u0 += v * (v < 0.5)
+// 			u1 += v * (v >= 0.5)
+// 			var0 += v * (v < 0.5) * (v < 0.5)
+// 			var1 += v * (v >= 0.5) * (v >= 0.5)
+// 		}
+// 	}
+
+// 	// Calculate the Otsu threshold.
+// 	var threshold float64
+// 	for t := 0.0; t <= 1.0; t += 0.01 {
+// 		var m0, m1 float64
+// 		m0 = u0 / w0
+// 		m1 = u1 / w1
+// 		var varBetween float64
+// 		varBetween = w0 * w1 * (m0 - m1) * (m0 - m1) / total
+// 		if varBetween > var0 {
+// 			var0 = varBetween
+// 			threshold = t
+// 		}
+// 	}
+
+// 	// Set the pixels to black if they are below the threshold, and white otherwise.
+// 	for y := 0; y < height; y++ {
+// 		for x := 0; x < width; x++ {
+// 			// Get the pixel value.
+// 			v := img.At(x, y).Y
+
+// 			// Set the pixel.
+// 			if v < threshold {
+// 				thresholded.At(x, y) = color.Black
+// 			} else {
+// 				thresholded.At(x, y) = color.White
+// 			}
+// 		}
+// 	}
+
+// 	// Return the thresholded image.
+// 	return thresholded
+// } // OtsuThreshold
